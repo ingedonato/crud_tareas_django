9bdd915204247e7fea262938a23264bd5547bf02
ingedonato <ingedonatomc@gmail.com>
2023-03-28 15:18:52 -0400

    segundo Commit

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
-def helloworld(request):
-    return render(request, 'signup.html', {
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
         'form': UserCreationForm
-        })
+    })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            # Register User
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse('Usuario creado satisfactoriamente...')
+            except:
+                return HttpResponse('El Usuario ya existe...')
+            
+        return HttpResponse('Password no coinciden...')
+
+        print(request.POST)
+        print('Obteniendo datos...')
+
+    
